1351ec3dd68f0253dadf0032032b31e6c77bdfd2
Sang Lee <lsmcap91@gmail.com>
2020-01-27 20:33:26 -0500

    7.109 - using PropTypes

--- src/components/Books/Book/Book.js
+++ src/components/Books/Book/Book.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import classes from './Book.css';
 import withClass from '../../../hoc/withClass';
+
 
 
 class Book extends Component {
@@ -8,12 +10,22 @@ class Book extends Component {
     console.log('[Book.js] rendering...')
     return (
         <Fragment>
-            <p onClick={this.props.click}>This book is called "{this.props.title}" and have {this.props.totalPage} pages! Currently read {this.props.currentPage} pages. </p>
+            <p onClick={this.props.click}>This book is called "{this.props.title}" 
+                and have {this.props.totalPage} pages! Currently read 
+                {this.props.currentPage} pages. </p>
             <p>{this.props.children}</p>
             <input type="text" onChange={this.props.changed} value={this.props.title}/>
         </Fragment>
         );
     }
+};
+
+Book.propTypes = {
+    click: PropTypes.func,
+    title: PropTypes.string,
+    currentPage: PropTypes.number,
+    totalPage: PropTypes.number,
+    changed: PropTypes.func
 };
 
 export default withClass(Book,classes.Book);
